813fbbf4f36697f7c509ae5b1aad7bcd56f1c150
leandramusto@gmail.com <leandramusto@gmail.com>
2024-02-23 17:19:12 +0100

    add css (need to complete), add cost ims, cycle, class list and inner creating murkup, initialize the variable to keep track of index of the current img, evenntclick to the button nnext,

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,5 +9,26 @@ const images = [
   "img/04.jpg",
   "img/05.jpg",
 ];
+console.log(itemsContainer);
+//I create the cyle to add elements in HTMLA a loop iterates
+for (let i = 0; i < images.length; i++) {
+  const photo = images[i];
+  let item = document.createElement("div");
 
-//I create the cyle to add elements in HTML
+  item.classList.add("item");
+  item.innerHTML = `<img src="${photo}"/>`;
+
+  itemsContainer.append(item);
+  //Adds the "active" class to the initially displayed image.
+  if (i === 0) {
+    item.classList.add("active");
+  }
+}
+//Initialization of a variable to keep track of the index of the currently displayed image
+let currentImageIndex = 0;
+
+const items = document.querySelectorAll(".item");
+//aggiungo evento click al pulsante next
+const btnNext = document.querySelector(".btn-next");
+
+btnNext.addEventListener('click', function() {
